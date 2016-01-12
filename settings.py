5ab0a88bfaa3c2f6bf6ef826089da40cf3cec0ce
Zach Swift <zachary.swift@healthways.com>
2016-01-12 00:26:36 -0600

    add settings file to public repo

---
+++ settings.py
@@ -0,0 +1,8 @@
+settings = {
+    "OPEN_WEATHER_API_KEY": '************************',
+    'TWILIO_ACCOUNT_SID': "*************************",
+    'TWILIO_AUTH_TOKEN': "***********************",
+    'CITY_ID': '4644585',
+    "TWILIO_FROM_NUMBER": '+15554443333',
+    "NUMBERS_TO_TEXT": ['+18887776666']
+}
